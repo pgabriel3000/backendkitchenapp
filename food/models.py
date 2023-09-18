89a4f006551e0a63a73602eb76c1bd653591aa5f
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-09-18 15:10:37 -0600

    cambiando modelos

--- food/models.py
+++ food/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Food (models.Model):
     name = models.TextField()
     price = models.IntegerField()
-    img = models.IntegerField()
-    description = models.IntegerField()
+    img = models.TextField()
+    description = models.TextField()
